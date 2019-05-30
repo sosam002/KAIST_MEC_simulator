7b1f6e3c8310448e6a1b17f0624ba3960a6f902c
sosam002 <elasteam88@gmail.com>
2019-05-31 03:39:08 +0900

    network_update

--- mecs/rl/utilities.py
+++ mecs/rl/utilities.py
@@ -1,13 +1,13 @@
 import tensorflow as tf
 
+# 임시로
+def local_energy_consumption(used_cpu, used_tx=0):
+    return used_cpu^2+usex_tx
 
+def offload_cost(used_tx, workloads):
+    used_cpu = np.array(used_tx)*np.array(workloads)
+    return used_cpu^2
 
-class agent(self):
-    pass
-
-def policy():
-    pass
-
-
-def reward():
-    pass
+def queue_size_penalty(queue_lengths, max_queue_lengths):
+    remaind_queues = np.square(max_queue_lengths-queue_lengths)
+    return np.sum(remained_queues)
