38f3c318bd575e4e7ac73c36a30e1270a2216cf2
sosam002 <elasteam88@gmail.com>
2019-05-31 01:56:09 +0900

    minor_modification

---
+++ mecs/sosam_main.py
@@ -0,0 +1,97 @@
+import json
+import logging
+import os
+import shutil
+
+# from mecs
+import scheduler, config
+# from wholemap import WholeMap
+
+from servernode_w_queue import ServerNode
+
+from applications import *
+from channels import *
+
+logger = logging.getLogger(__name__)
+
+def init_for_sosam(edge_capability, cloud_capability, channel, *applications):
+    edge_server = ServerNode(edge_capability)
+    cloud_server = ServerNode(server_capability)
+    edge_server.add_link(cloud_server, channel)
+    edge_server.make_application_queues(*applications)
+    cloud_server.make_application_queues(*applications)
+
+
+def main():
+
+    edge_capability = 30000
+    cloud_capability = 300000  # clock per tick
+    channel = WIRED
+    applications = (AR, VR)
+    task_rate = 10000 # 들어갈만한 다른 곳을 찾고 싶다.ㅠㅠ
+    # schedule_method = scheduler.RRScheduler().schedule
+    # my_map.add_server(150, 150, server_capability, schedule_method)
+    log_dir = 'result_sosam'
+
+    mobile_log = {}
+    if os.path.isdir(log_dir):
+        shutil.rmtree(log_dir)
+    os.mkdir(log_dir)
+
+    init_for_sosam(edge_capability, cloud_capability, channel, *applications)
+
+    cloud_policy = [0.5, 0.5]
+
+    for t in range(200001):
+        edge_server.random_task_generation(task_rate)
+        print(edge_server.queue_list[8].tasks, edge_server.queue_list[7].tasks)
+        print(cloud_server.queue_list[8].tasks, cloud_server.queue_list[7].tasks)
+
+        '''
+        server node state (queue length)
+        edge node stat (queue length)
+        server node와 edge node의 cpu랑 통신 rate spec도 입력받아야할 듯
+        저장해놔야 함.
+        '''
+
+        '''
+        action받아오기! alpha, beta 받는 자리
+        '''
+        # 지금은 policy가 없으니까 내맘대로
+        alpha = [0.5,0.5]
+        beta = [0.5,0.5]
+
+        used_edge_cpu = edge_server.do_tasks(alpha)
+        used_tx, task_to_be_offloaded = edge_server.offload_tasks(beta, cloud_server.get_uuid())
+        cloud_server.offloaded_tasks(task_to_be_offloaded)
+        used_cloud_cpu = cloud_server.do_tasks(cloud_policy)
+        # used_tx, task_to_be_offloaded = edge_server.do_and_offload( alpha, beta, cloud_server.get_uuid() )
+        print("used_edge_cpu {}, used_tx {}, used_cloud_cpu {}".format(used_edge_cpu, used_tx, used_cloud_cpu))
+        # import pdb; pdb.set_trace()
+        '''
+        used_cpu랑 offload 정보 받아서 reward 받는 자리
+        '''
+        '''
+        server node state (queue length)
+        edge node stat (queue length)
+        server node와 edge node의 cpu랑 통신 rate spec도 입력받아야할 듯
+        '''
+        '''
+        네트워크에 (s,a,r,s') 집어넣고,
+        '''
+
+        # if t % 1000 == 0:
+        #     logger.info(
+        #         "================= << [%d,%d] second >> =================",
+        #         t // 1000, t // 1000 + 1)
+        #     logger.debug(json.dumps(mobile_log))
+        #     mobile_log = {}
+        # mobile_log[t] = my_map.simulate_one_time(t)
+
+
+    # my_map.print_all_mobiles()
+
+
+if __name__ == "__main__":
+    config.initialize_mecs()
+    main()
--- mecs/task.py
+++ mecs/task.py
@@ -2,9 +2,9 @@ import uuid
 import copy
 
 class Task(object):
-    def __init__(self, client_index, server_index, application_type, data_size):
-        self.client_index = client_index
-        self.server_index = server_index
+    def __init__(self, application_type, data_size, client_index = None, server_index = None ):
+        self.client_index = client_index # 이게 오프로드된 작업이라면 요청한 client가 있지
+        self.server_index = server_index # 이게 여기서 오프로드시켰다면 오프로드 시킨 server가 있지.
         self.application_type = application_type
         self.data_size = data_size
         self.is_start = False
@@ -13,6 +13,10 @@ class Task(object):
         self.received_data_size = 0
         self.parent_uuid = None
         self.child_uuid = None
+        # arrival rate 분석할 때 최근 몇 초 안에 도착한 arrival을 기록해야 함.
+        self.arrival_timestamp = None
+        self.start_timestamp = None # 혹시 몰라서. task별 waiting time 필요할 수도 있음.
+        self.end_timestamp = None # 마찬가지.
 
     def get_uuid(self):
         return self.uuid.hex
@@ -24,3 +28,26 @@ class Task(object):
         new_task.data_size = offload_data_bits
         self.data_size = self.data_size-offload_data_bits
         return new_task
+
+    def is_client_index(self):
+        return bool(self.client_index)
+
+    def is_server_index(self):
+        return bool(self.server_index)
+
+    '''
+    self.client_index self.server_index
+    0 None , None : 작업 끝나면 끝. 원래 내거
+    1 None , is : 작업 끝나면, offload된 거 기다려야 함.
+    2 is , None : 작업 끝나면, downlink로 보내야 함. (downlink queue로 가야 함)
+    3 is , is : 작업 끝나면, 어딘가에서 output data를 들고 기다리다가... uplink에서 오는거 기다렸다가 downlink로 보내야 함.(downlink queue로 가야 함)
+    '''
+    def offload_state(self):
+        if self.is_client_index() and self.is_server_index():
+            return 3
+        elif self.is_client_index() and (not self.is_server_index()):
+            return 2
+        elif (not self.is_client_index()) and self.is_server_index():
+            return 1
+        else :
+            return 0
