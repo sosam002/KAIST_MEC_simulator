bc3803bfd412a44610ca8d344153b64d5225bc6c
sosam002 <elasteam88@gmail.com>
2022-05-28 04:38:31 +0900

    Update environment_gym.py

--- MECS_gym/environment_gym.py
+++ MECS_gym/environment_gym.py
@@ -8,14 +8,11 @@ import numpy as np
 from servernode_w_appqueue import ServerNode as ANode
 from servernode_w_totalqueue import ServerNode as TNode
 from channels import Channel
-from constants import GHZ, WIRED
-from cost_functions_cores import get_local_power_cost
+from constants import *
+from applications import *
 
 
-register(id='MEC-v1', entry_point=MEC_v1)
-
-class MEC_v1(gym.Env):
-    # def __init__(self, tq_nodes, aq_nodes, channels, link_infos, task_rate=1, use_beta=True, cost_type=1, max_episode_steps=5000):
+class MEC(gym.Env):
     def __init__(self, args):
         super().__init__()
         self.reset_info = args
@@ -23,7 +20,7 @@ class MEC_v1(gym.Env):
 
 
         # it depends on state design. Edit this and get_state() function.
-        state = self.get_status()
+        state = self._get_obs()
         self.state_dim = len(state)
         self.observation_space = Box(low=-np.inf, high=np.inf, shape=(len(state),))
 
@@ -33,11 +30,7 @@ class MEC_v1(gym.Env):
             self.action_dim += len(client.get_applications())+1
         if self.use_beta:
             self.action_dim *=2
-        self.action_space = Box(low=-np.inf, high=np.inf, shape=(self.action_dim,))
-
-
-    def render(self):
-        raise NotImplementedError()
+        self.action_space = Box(low=-2, high=2, shape=(self.action_dim,))
 
 
     def add_link(self, client, server, up_channel, down_channel=None):
@@ -83,6 +76,7 @@ class MEC_v1(gym.Env):
         self.task_rate = self.reset_info['task_rate'] #time_delta
         self.cost_type = self.reset_info['cost_type']
         self.max_episode_steps = self.reset_info['max_episode_steps']
+        self.scale = self.reset_info['scale']
         self.use_beta = True
         self.applications=set()
         self.timestamp = 0
@@ -93,13 +87,19 @@ class MEC_v1(gym.Env):
         self.channels = dict()
         self.links = list()
 
+        self.lambdas = list()
 
         for node in self.reset_info['tq_nodes']:
             node['uuid']= self.add_tq_node(node['id'], node['num_cores'], node['single_clk'], node['movable'])
 
         for node in self.reset_info['aq_nodes']:
-            node['uuid']= self.add_aq_node(node['id'], node['num_cores'], node['single_clk'], node['applications'], node['is_random_task_generating'], node['offload_type'], node['movable'])
+            node['uuid']= self.add_aq_node(node['id'], node['num_cores'], node['single_clk'], tuple(node['applications']), node['is_random_task_generating'], node['offload_type'], node['movable'])
+            for app in node['applications']:
+                import pdb; pdb.set_trace()
+                self.lambdas.append(arrival_bits(app, dist='deterministic') * get_info(app, "popularity"))
             self.applications.update(node['applications'])
+
+        self.lambdas = np.array(self.lambdas) * self.task_rate / self.scale
 
         for channel in self.reset_info['channels']:
             channel['uuid']= self.add_channel(channel['id'], channel['channel_type'], channel['pathloss'], channel['lf'], channel['sf_type'], channel['sf_factor'], channel['rate'], channel['op_freq'])
@@ -112,23 +112,28 @@ class MEC_v1(gym.Env):
 
             self.add_link(client, server, self.channels[link['channel_id']])
 
-    def reset(self):
+    def _reset(self):
         self.initialize()
-        reset_state = self.get_status(scale=GHZ)
+        self.before_arrival = self.get_edge_qlength()
+        _, failed_to_generate, _ = self._step_generation()
+        reset_state = self._get_obs()
+        reset_state[3:6] = self.before_arrival # q(t)
+        reset_state = self._get_obs()
+        reset_state[-1] = 0.0
         return reset_state
 
-    def get_status(self, scale=GHZ, involve_link=False):
+    def _get_obs(self, involve_link=False):
         edge_state, cloud_state, link_state = list(), list(), list()
         # failed_to_offload, failed_to_generate = 0, 0
         for client in self.clients.values():
-            temp_state = client.get_status(self.timestamp, scale=scale)
+            temp_state = client._get_obs(self.timestamp, scale=self.scale)
             edge_state += temp_state
             # failed_to_generate += sum(temp_state[24:32])
 
         state = edge_state
         if self.use_beta:
             for server in self.servers.values():
-                temp_state = server.get_status(self.timestamp, scale=scale)
+                temp_state = server._get_obs(self.timestamp, scale=self.scale)
                 cloud_state += temp_state
                 # failed_to_offload +=sum(temp_state[24:32])
             for link in self.links:
@@ -139,48 +144,82 @@ class MEC_v1(gym.Env):
         else:
             state = edge_state + cloud_state
 
-        return np.array(state)#, failed_to_offload, failed_to_generate
+        return np.array(state[:-3])#, failed_to_offload, failed_to_generate
 
     # several clients, several servers not considered. (step, _step_alpha, _step_beta)
     # def step(self, action, cloud, use_beta=True, generate=True):
     def step(self, action):
-        q0, failed_to_generate, q1 = self._step_generation()
+        # print(action)
+        q0 = np.array(self.before_arrival) #q(t)
+        action = 5 * action
+        start_state = self._get_obs()
+        q1=np.array(self.get_edge_qlength()) #q(t)+a(t)
+        a_t = start_state[3:6]
         action_alpha, action_beta, usage_ratio = list(), list(), list()
         if self.use_beta:
+            # action_alpha = np.array(list(action[:int(len(action)/2)]) + [0.0]).reshape(1,-1)
+            # action_beta = np.array(list(action[int(len(action)/2):]) + [0.0]).reshape(1,-1)
             action_alpha = action.flatten()[:int(self.action_dim/2)].reshape(1,-1)
             action_beta = action.flatten()[int(self.action_dim/2):].reshape(1,-1)
+            ### softmax here
+            action_beta = softmax_1d(action_beta)
         else:
             action_alpha = action
+        ### softmax here
+        action_alpha = softmax_1d(action_alpha)
+        # action_alpha = np.array([[0,0,0,1]])
+        # action_beta = np.array([[0,0,0,1]])
 
-        used_edge_cpus, inter_state, q2 = self._step_alpha(action_alpha)
-        # used_cloud_cpus, new_state, failed_to_offload, q3 = self._step_beta(action_beta, np.array(cloud).reshape(-1,len(cloud)))
-        # used_cloud_cpus, new_state = self._step_beta(action_beta, np.array(cloud).reshape(-1,len(cloud)))
-        used_cloud_cpus, new_state = self._step_beta(action_beta)
-        # fail_cost = self.get_fail_cost(failed_to_offload, failed_to_generate)
+        if self.timestamp%1000==0:
+            print("--------------------------------------------")
+            print("action", action)
+            print("action_alpha", action_alpha)
+            print("action_beta", action_beta)
+            print("q_length",q1)
 
-        # cost = self.get_cost(used_edge_cpus, used_cloud_cpus, q0, q3, failed_to_offload, failed_to_generate)
-        cost = self.get_cost(used_edge_cpus, used_cloud_cpus, q0, q2)
+
+        # print("--------------------------------------------")
+        # print("action", action)
+        # print("action_alpha", action_alpha)
+        # print("action_beta", action_beta)
+        # print(used_edge_cpus)
+        # print(used_cloud_cpus)
+        used_edge_cpus, inter_state = self._step_alpha(action_alpha)
+        ## print("action_beta", action_beta)
+        # if self.timestamp < 500000:
+        #     cost_type = 1000
+        # else:
+        #     cost_type = np.copy(self.cost_type)
+
+        used_cloud_cpus, new_state, q_last = self._step_beta(action_beta)
+        self.before_arrival = q_last #q(t+1)
+        q2 = np.array(q_last) #q(t+1)=q(t)+a(t)-b(t)
+        at_bt=q2-q0
+        bt = q2 - q1
+        _, failed_to_generate, _ = self._step_generation()
+
+        new_state= self._get_obs()
+        new_state[3:6] = q2
+        q3 = new_state[6:9] #q(t+1)+a(t+1)
+
+        cost = self.get_cost(used_edge_cpus, used_cloud_cpus, q0, bt)
+        new_state[-1] = list(used_cloud_cpus.values())[0]/self.scale/216
         self.timestamp += 1
         if self.timestamp == self.max_episode_steps:
-            return new_state, -cost, True, {}
-        # return new_state, cost, failed_to_offload+failed_to_generate
-        return new_state, -cost, False, {}
+            return new_state, -cost, 1, {"cloud_cpu_used":start_state[-1]}
+        return new_state, -cost, 0, {"cloud_cpu_used":start_state[-1]}
 
     def _step_alpha(self, action):
         # initial_qlength= self.get_total_qlength()
         used_edge_cpus = collections.defaultdict(float)
         action = action.flatten()[:-1].reshape(1,-1)
-        if self.timestamp%1000==0:
-            print("alpha", 1-sum(sum(action)))
         for client_id, alpha in list(zip(self.clients.keys(), action)):
             used_edge_cpus[client_id] = self.clients[client_id].do_tasks(alpha)
+        state = self._get_obs()
 
-        # state, _, _ = self.get_status()
-        state = self.get_status(scale=GHZ)
-        # after_qlength = self.get_total_qlength()
-        after_qlength = self.get_edge_qlength(scale=GHZ)
-
-        return used_edge_cpus, state, after_qlength
+        if self.timestamp%1000==0:
+            print("alpha", 1-sum(sum(action)))
+        return used_edge_cpus, state
 
 
     # def _step_beta(self, action, action_cloud):
@@ -189,140 +228,82 @@ class MEC_v1(gym.Env):
         tasks_to_be_offloaded = collections.defaultdict(dict)
         used_cloud_cpus = collections.defaultdict(float)
         action = action.flatten()[:-1].reshape(1,-1)
-        if self.timestamp%1000==0:
-            print("beta", 1-sum(sum(action)))
         # 모든 client 객체에 대해 각 client의 상위 node로 offload하기
         # 각 client는 하나의 상위 노드를 가지고 있다고 가정함......?
+        q_before = self.get_edge_qlength()
+        # print(q_before)
         for client, beta in list(zip(self.clients.values(), action)):
             higher_nodes = client.get_higher_node_ids()
             for higher_node in higher_nodes:
-                # import pdb; pdb.set_trace()/
                 used_tx, task_to_be_offloaded, failed = client.offload_tasks(beta, higher_node)
                 used_txs[higher_node].append(used_tx)
                 tasks_to_be_offloaded[higher_node].update(task_to_be_offloaded)
-
-        # server_action = dict(zip(self.servers.keys(), action_cloud))
+        q_last = self.get_edge_qlength()
+        # print(np.array(q_last)-np.array(q_before))
+        # print((np.array(q_last)-np.array(q_before))*np.array([10000,20000,40000]))
+        s1 = self._get_obs()
         for server_id, server in self.servers.items():
-            # import pdb; pdb.set_trace()
+            # print("s1",server.task_queue.get_cpu_needs(scale=1))
             server.offloaded_tasks(tasks_to_be_offloaded[server_id], self.timestamp)
+            # print("s2",server.task_queue.get_cpu_needs(scale=1))
+            s2 = self._get_obs()
             used_cloud_cpus[server_id] = server.do_tasks()
+            used_cloud_cpus[server_id] = (s2-s1)[-2]*GHZ
 
-        # after_qlength = self.get_total_qlength()
-        # state, failed_to_offload, _ = self.get_status()
-        state = self.get_status(scale=GHZ)
-        # return used_cloud_cpus, state, failed_to_offload, after_qlength
-        return used_cloud_cpus, state
+
+        state = self._get_obs()
+        if self.timestamp%1000==0:
+            print("beta", 1-sum(sum(action)))
+        return used_cloud_cpus, state, q_last
 
     def _step_generation(self):
-        # initial_qlength= self.get_total_qlength()
         initial_qlength= self.get_edge_qlength()
-        if not self.silence:
-            print("###### random task generation start! ######")
         for client in self.clients.values():
-            arrival_size, failed_to_generate = client.random_task_generation(self.task_rate, self.timestamp, *self.applications)
-        if not self.silence:
-            print("###### random task generation ends! ######")
-        # after_qlength = self.get_total_qlength()
-        after_qlength = self.get_edge_qlength(scale=GHZ)
+            arrival_size, failed_to_generate = client.random_task_generation(self.task_rate, self.timestamp, self.applications)
+
+        after_qlength = self.get_edge_qlength()
 
         return initial_qlength, failed_to_generate, after_qlength
 
 
-    def get_edge_qlength(self, scale=1):
+    def get_edge_qlength(self):
         qlengths = list()
         for node in self.clients.values():
             for _, queue in node.get_queue_list():
-                qlengths.append( queue.get_length(scale) )
+                qlengths.append( queue.get_length(scale=self.scale) )
         return qlengths
 
-    def get_cloud_qlength(self, scale=1):
+    def get_cloud_qlength(self):
         qlengths = list()
         for node in self.servers.values():
-            qlengths.append(node.get_task_queue_length(scale))
+            qlengths.append(node.get_task_queue_length(scale=self.scale))
         return np.array(qlengths)
 
+    def get_cost(self, used_edge_cpus, used_cloud_cpus,before, bt, failed_to_offload=0, failed_to_generate=0):
 
-    def get_cost_old(self, used_edge_cpus, used_cloud_cpus, before, after, failed_to_offload=0, failed_to_generate=0):
-        # drift_cost = get_drift_cost2(np.array(before),np.array(after),self.empty_reward)
-        # import pdb; pdb.set_trace()
+        def compute_cost_fct(cores, cpu_usage):
+            return cores*(cpu_usage/400/GHZ/cores)**3
 
-        lyap_drift_cost = sum(np.array(after)**2)-sum(np.array(before)**2)
-        if lyap_drift_cost >0:
-            lyap_drift_cost=np.sqrt(lyap_drift_cost)
-        else:
-            lyap_drift_cost=-np.sqrt(-lyap_drift_cost)
-        edge_drift_cost = np.sqrt(sum(np.array(after)**2))
-        #
-        # after = np.array(after)
-        # before = np.array(before)
-        # condition = after-before
-        # drift_cost = [sum((after+ (condition>0)*before)**2)]*100
+        edge_drift_cost = sum( 2*before*(self.lambdas+bt)+(self.lambdas+bt)**2 )
 
-        # fail_cost = get_fail_cost(failed_to_offload, failed_to_generate)*drift_cost[0]
-        edge_computation_cost = 0
-        for used_edge_cpu in used_edge_cpus.values():
-            # local_cost += 10*(used_edge_cpu/10)**3
-            edge_computation_cost += get_local_power_cost(used_edge_cpu)
-        edge_computation_cost /= (900000*64*GHZ**3)
-
-        cloud_payment_cost = 0
-        for used_cloud_cpu in used_cloud_cpus.values():
-            cloud_payment_cost += 54*(used_cloud_cpu/54)**3
-        cloud_payment_cost /= (900000*64*GHZ**3)
-        # import pdb; pdb.set_trace()
-        cloud_payment_cost += sum(self.get_cloud_qlength(scale=GHZ)**2)
-
-        # import pdb; pdb.set_trace()
-
-        if self.cost_type==1:
-            return edge_drift_cost + edge_computation_cost + cloud_payment_cost
-        elif self.cost_type==2:
-            return edge_drift_cost + edge_computation_cost + 10*cloud_payment_cost
-        elif self.cost_type==3:
-            return edge_drift_cost + 1000*edge_computation_cost + 10*1000*cloud_payment_cost
-        elif self.cost_type==4:
-            return edge_drift_cost + 10*1000*edge_computation_cost + 1000*cloud_payment_cost
-        elif self.cost_type==5:
-            return edge_drift_cost + 100*1000*edge_computation_cost + 1000*cloud_payment_cost
-        else:
-            return lyap_drift_cost + edge_computation_cost + cloud_payment_cost
-
-        # return total_cost(used_edge_cpus, used_cloud_cpus, drift_cost, option=self.cost_type)# + fail_cost
-        # return drift_cost[0]
-    def get_cost(self, used_edge_cpus, used_cloud_cpus, before, after, failed_to_offload=0, failed_to_generate=0):
-
-        # import pdb; pdb.set_trace()
-        # for 3 apps
-        edge_drift_cost = sum(np.array(after)**2)
-        # for 8 apps
-        # edge_drift_cost = 10*sum(np.array(after)**2)
 
         edge_computation_cost = 0
         for used_edge_cpu in used_edge_cpus.values():
-            if self.cost_type==10 or self.cost_type==11 or self.cost_type==14 or self.cost_type==16 or self.cost_type>99:
-                edge_computation_cost += 10*(used_edge_cpu/10)**3
-            else: # 12, 13, 15
-                edge_computation_cost += get_local_power_cost(used_edge_cpu)
-        edge_computation_cost /= (900000*64*GHZ**3)
+            edge_computation_cost += compute_cost_fct(10,used_edge_cpu)
 
         cloud_payment_cost = 0
         for used_cloud_cpu in used_cloud_cpus.values():
-            # for 3 apps
-            cloud_payment_cost += 54*(used_cloud_cpu/54)**3
-            # for 8 apps
-            # cloud_payment_cost += 45*(used_cloud_cpu/45)**3
-        cloud_payment_cost /= (900000*64*GHZ**3)
+            cloud_payment_cost += compute_cost_fct(54,used_cloud_cpu)
 
 
-        if self.cost_type==10 or self.cost_type==12:
-            return 100*edge_drift_cost + 1*edge_computation_cost + 50*cloud_payment_cost
-        elif self.cost_type==11 or self.cost_type==13:
-            return 100*edge_drift_cost + 50*edge_computation_cost + 1*cloud_payment_cost
-        elif self.cost_type==14 or self.cost_type==15:
-            return 100*edge_drift_cost + 100*edge_computation_cost + 1*cloud_payment_cost
-        elif self.cost_type==16 or self.cost_type==17:
-            return 100*edge_drift_cost + 100*edge_computation_cost + 0.1*cloud_payment_cost
-        elif self.cost_type>99:
-            return self.cost_type*edge_drift_cost/100 + 10*edge_computation_cost + 10*cloud_payment_cost
-        else:
-            return (10**self.cost_type)*edge_drift_cost/100 + 10*edge_computation_cost + 10*cloud_payment_cost
+        if self.timestamp%1000==0:
+            print("used cpu edge : ", used_edge_cpus.values())
+            print("used cpu cloud : ", used_cloud_cpus.values())
+            print("edge power : ", edge_computation_cost)
+            print("cloud power : ", cloud_payment_cost)
+            print("power * cost : ", self.cost_type*(edge_computation_cost+cloud_payment_cost))
+            print("cost : ", edge_drift_cost+self.cost_type*(edge_computation_cost+cloud_payment_cost))
+            print("rew : ", -(edge_drift_cost+self.cost_type*(edge_computation_cost+cloud_payment_cost)))
+
+
+        return (edge_drift_cost+self.cost_type*(edge_computation_cost+cloud_payment_cost))
